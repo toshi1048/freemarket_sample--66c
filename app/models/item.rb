2de66be2215e9cc1a64d986562c5fdee7f508da9
tokunagakazuki <tokutyaa@yahoo.co.jp>
2020-01-17 14:24:55 +0900

    モデル設計

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,7 @@
 class Item < ApplicationRecord
+  belongs_to user, foreign_key: 'user_id'
+  belongs_to :category
+  belongs_to :bland
+  has_many :images
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, :first_name, :last_name, :first_name_rattle, presence: true
+  has_one :phone_number
+  has_one :address
+  has_one :card
+  
+  accepts_nested_attributes_for :phone_number, :address, :card
 end
