7e6c676baa353fc10d0af115ceb7bc6822995e22
tokunagakazuki <tokutyaa@yahoo.co.jp>
2020-01-18 01:24:16 +0900

    一覧表示、ブランド表示完了

--- app/controllers/toppage_controller.rb
+++ app/controllers/toppage_controller.rb
@@ -1,14 +1,19 @@
 class ToppageController < ApplicationController
   def index
-    @ladies      = Item.where(category_id:  1..200).limit(8)
-    @mens        = Item.where(category_id:201..345).limit(8)
-    @kids        = Item.where(category_id:346..480).limit(8)
-    @interior    = Item.where(category_id:481..624).limit(8)
-    @game        = Item.where(category_id:625..684).limit(8)
-    @hobby       = Item.where(category_id:685..797).limit(8)
-    @cosmetics   = Item.where(category_id:798..897).limit(8)
-    @electronics = Item.where(category_id:898..983).limit(8)
-    @sports      = Item.where(category_id:984..1000).limit(8)
+    @ladies        = Item.where(category_id:  1..200).recent
+    @mens          = Item.where(category_id:201..345).recent
+    @kids          = Item.where(category_id:346..480).recent
+    @interior      = Item.where(category_id:481..624).recent
+    @game          = Item.where(category_id:625..684).recent
+    @hobby         = Item.where(category_id:685..797).recent
+    @cosmetics     = Item.where(category_id:798..897).recent
+    @electronics   = Item.where(category_id:898..983).recent
+    @sports        = Item.where(category_id:984..1000).recent
+    @chanel        = Item.where(brand_id:1).recent
+    @louis_vuitton = Item.where(brand_id:2).recent
+    @supreme       = Item.where(brand_id:3).recent
+    @nike          = Item.where(brand_id:4).recent
+
 
     
     # @category_parent_array = ["---"]
--- app/models/item.rb
+++ app/models/item.rb
@@ -8,5 +8,5 @@ class Item < ApplicationRecord
   enum category_l: {"----":0,"レディース":1,"メンズ":2,"ベビー・キッズ":3,"コスメ・香水・美容":4,"インテリア・住まい・小物":5,"本・音楽・ゲーム":6,"おもちゃ・ホビー・グッズ":7,"家電・スマホ・カメラ":8,"スポーツ・レジャー":9,"ハンドメイド":10,"チケット":11,"自動車・オートバイ":12,"その他":13}
   enum sales_status: {"出品中":1,"公開停止":2,"取引中":3,"売却済み":4}
 
-  scope :recent, -> {order('id DESC').limit(4).where.not(sales_status:"公開停止")}
+  scope :recent, -> {order('id DESC').limit(4)}
 end
