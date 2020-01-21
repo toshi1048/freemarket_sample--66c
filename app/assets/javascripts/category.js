a790254dd86569b1249c5bc9b55155fe0149e24e
hiroshi0918 <58413665+hiroshi0918@users.noreply.github.com>
2020-01-21 16:37:15 +0900

    ブランドへの入力フォーム作成、コメントアウト削除

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -1,10 +1,8 @@
 $(function(){
-  // カテゴリーセレクトボックスのオプションを作成
   function appendOption(category){
     var html = `<option value="${category.id}" data-category="${category.id}">${category.name}</option>`;
     return html;
   }
-  // 子カテゴリーの表示作成
   function appendChidrenBox(insertHTML){
     var childSelectHtml = '';
     childSelectHtml = `
@@ -14,7 +12,6 @@ $(function(){
                           <select>`;
     $('.sell-category-box').append(childSelectHtml);
   }
-  // 孫カテゴリーの表示作成
   function appendGrandchidrenBox(insertHTML){
     var grandchildSelectHtml = '';
     grandchildSelectHtml = `    <select class="sell__item-categry" id="grandchild_category" name="category_id">
@@ -23,9 +20,8 @@ $(function(){
                                 </select>`;
     $('.sell-category-box').append(grandchildSelectHtml);
   }
-  // 親カテゴリー選択後のイベント
   $('#parent_category').on('change', function(){
-    var parentCategory = document.getElementById('parent_category').value; //選択された親カテゴリーの名前を取得
+    var parentCategory = document.getElementById('parent_category').value;
     if (parentCategory != "---"){ //親カテゴリーが初期値でないことを確認
       $.ajax({
         url: 'get_category_children',
@@ -36,8 +32,6 @@ $(function(){
       .done(function(children){
         $('#child_category').remove(); //親が変更された時、子以下を削除するする
         $('#grandchild_category').remove();
-        $('#size_wrapper').remove();
-        $('#brand_wrapper').remove();
         var insertHTML = '';
         children.forEach(function(child){
           insertHTML += appendOption(child);
@@ -48,16 +42,13 @@ $(function(){
         alert('カテゴリー取得に失敗しました');
       })
     }else{
-      $('#child_category').remove(); //親カテゴリーが初期値になった時、子以下を削除するする
+      $('#child_category').remove();
       $('#grandchild_category').remove();
-      $('#size_wrapper').remove();
-      $('#brand_wrapper').remove();
     }
   });
-  // 子カテゴリー選択後のイベント
   $('.sell-category-box').on('change', '#child_category', function(){
-    var childId = $('#child_category option:selected').data('category'); //選択された子カテゴリーのidを取得
-    if (childId != "---"){ //子カテゴリーが初期値でないことを確認
+    var childId = $('#child_category option:selected').data('category');
+    if (childId != "---"){
       $.ajax({
         url: 'get_category_grandchildren',
         type: 'GET',
@@ -66,9 +57,7 @@ $(function(){
       })
       .done(function(grandchildren){
         if (grandchildren.length != 0) {
-          $('#grandchild_category').remove(); //子が変更された時、孫以下を削除するする
-          $('#size_wrapper').remove();
-          $('#brand_wrapper').remove();
+          $('#grandchild_category').remove();
           var insertHTML = '';
           grandchildren.forEach(function(grandchild){
             insertHTML += appendOption(grandchild);
@@ -80,9 +69,7 @@ $(function(){
         alert('カテゴリー取得に失敗しました');
       })
     }else{
-      $('#grandchild_category').remove(); //子カテゴリーが初期値になった時、孫以下を削除する
-      $('#size_wrapper').remove();
-      $('#brand_wrapper').remove();
+      $('#grandchild_category').remove();
     }
   });
 });
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :category
-  belongs_to :bland
+  belongs_to :brand
   has_many :images
   accepts_nested_attributes_for :images, allow_destroy: true
   belongs_to :saler, class_name: "User"
