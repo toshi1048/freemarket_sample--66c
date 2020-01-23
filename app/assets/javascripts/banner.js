4a52dc358b937a43b6faf458cf102f5bd72d598e
st-yk <meisdcw@gmail.com>
2020-01-23 18:42:51 +0900

    完成

--- app/assets/javascripts/banner.js
+++ app/assets/javascripts/banner.js
@@ -5,7 +5,7 @@ $(function() {
     nextArrow:'<i class="fa fa-angle-right arrow arrow-right"></i>',
     dots: false,
     autoplay: true,
-    autoplaySpeed: 4000,
+    autoplaySpeed: 3000,
     auroplaySpeed: 500
   });
 });
