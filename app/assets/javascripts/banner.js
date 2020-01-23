05ccff77bf6bef1840fa8f20c5d09e9a6c5770ae
st-yk <meisdcw@gmail.com>
2020-01-23 18:04:50 +0900

    add

---
+++ app/assets/javascripts/banner.js
@@ -0,0 +1,12 @@
+
+$(function() {
+  $('.a').slick({
+    prevArrow:'<i class="fa fa-angle-left arrow arrow-left"></i>',
+    nextArrow:'<i class="fa fa-angle-right arrow arrow-right"></i>',
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    auroplaySpeed: 500
+  });
+});
+
